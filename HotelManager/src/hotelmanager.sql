33a7f68087febc4808e8e6c1b76aa1846bbd32f3
26094 <26094@DESKTOP-ISBLF46>
2020-06-22 22:52:01 +0800

    测试模板

---
+++ HotelManager/src/hotelmanager.sql
@@ -0,0 +1,103 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.27 : Database - hotelmanager
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hotelmanager` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `hotelmanager`;
+
+/*Table structure for table `t_food` */
+
+DROP TABLE IF EXISTS `t_food`;
+
+CREATE TABLE `t_food` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `price` double NOT NULL,
+  `describe` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_food` */
+
+/*Table structure for table `t_foodorder` */
+
+DROP TABLE IF EXISTS `t_foodorder`;
+
+CREATE TABLE `t_foodorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `food_id` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_foodorder` */
+
+/*Table structure for table `t_room` */
+
+DROP TABLE IF EXISTS `t_room`;
+
+CREATE TABLE `t_room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roomnum` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `bednumber` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_room` */
+
+/*Table structure for table `t_roomorder` */
+
+DROP TABLE IF EXISTS `t_roomorder`;
+
+CREATE TABLE `t_roomorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ordertime` datetime NOT NULL,
+  `overtime` datetime NOT NULL,
+  `paystate` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_roomorder` */
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `sex` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `identity` varchar(20) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`username`,`password`,`sex`,`email`,`phone`,`identity`,`role`) values (1,'test','123','1','1111','1111','1111',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
